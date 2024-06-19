d07d042f75bc300c47b30f039df32104eb23fc92
valiknvk <novik.valik@mail.ru>
2024-06-19 14:09:35 +0300

    correct

---
+++ nix/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "jekyll website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = { self, systems, nixpkgs }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs (import systems) (system: f {
+        inherit system;
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
+    in
+    rec {
+      packages = forAllSystems ({ pkgs, ... }: rec {
+        default = pkgs.callPackage ./. { };
+
+        serve = let
+          port = 4000;
+        in pkgs.writeShellScriptBin "serve-website" ''
+          echo "Running in http://localhost:${toString port}"
+          ${nixpkgs.lib.getExe pkgs.webfs} -F -p ${toString port} -f index.html -r ${default}/public
+        '';
+      });
+
+      apps = forAllSystems ({ system, ... }: {
+        default = {
+          type = "app";
+          program = "${packages.${system}.serve}/bin/serve-website";
+        };
+      });
+
+      hydraJobs = forAllSystems ({ system, ... }: {
+        main = packages.${system}.default;
+      });
+    };
+}
